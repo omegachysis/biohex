62b871f4eee527ffcbb070645102d139c55fa6d0
Matthew Robinson <omegachysis@outlook.com>
2014-07-16 08:45:59 -0400

    starting engine

--- draw.py
+++ draw.py
@@ -1,5 +1,22 @@
 
 import pygame
+from pygame.locals import *
 
 pygame.init()
 
+class Screen(object):
+    def __init__(self, width, height, fullscreen=False):
+
+        self.canvas = None
+        if fullscreen:
+            self.canvas = pygame.display.set_mode((width, height), FULLSCREEN)
+        else:
+            self.canvas = pygame.display.set_mode((width, height))
+
+    def draw(self, surface, position):
+        self.canvas.blit(surface, position)
+
+    def update(self):
+        pygame.display.flip()
+
+    
