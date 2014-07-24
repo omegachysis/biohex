03d36ac1c66cdbca1a1d9e9b0772d92bbe174426
Matthew Robinson <omegachysis@outlook.com>
2014-07-24 13:35:25 -0400

    Documented main.py

--- main.py
+++ main.py
@@ -12,24 +12,34 @@ import bitLibrary
 
 pygame.init()
 
+# command to run on initialization
+INIT_STATEMENT = "runGraphics()"
+# use python profiler
+USE_PROFILER = False
+
 def runGraphics():
-    METRIC_WIDTH = 10
-    METRIC_HEIGHT = 10
-    METRIC_T = 3
+    METRIC_WIDTH = 10       # width from very left to very right of hexagon
+    METRIC_HEIGHT = 10      # height from very top to very bottom of hexagon
+    METRIC_T = 3            # distance from upper left boundary 
+                            #   corner to upper left corner of hexagon
     
     hexmech.setMetrics(
         width = 10, height = 10, t = 3)
 
+    # search ring cache for all hexagon rings and load them into hexmech.rings
     hexmech.loadRings()
 
+    # screen resolution.
     RES_WIDTH = 1280
     RES_HEIGHT = 720
 
+    # world width and height in hexagons
     WORLD_WIDTH = 400
     WORLD_HEIGHT = 200
     
     FULLSCREEN = False
 
+    # calculate size of on-screen canvas
     CANVAS_WIDTH = WORLD_WIDTH * (METRIC_WIDTH+METRIC_T*2) / 2
     CANVAS_HEIGHT = WORLD_HEIGHT * METRIC_HEIGHT
 
@@ -39,10 +49,15 @@ def runGraphics():
 
     engine = graphics.Engine(screen, world, ticksPerSecond = 10)
 
+    # important to do this after initialization
     life.Bit.world = world
+    
+    # random seed to start the world
+    SEED = 0
 
-    random.seed(0)
+    random.seed(SEED)
 
+    # RNA for ribosome.  details in bitLibrary.organelles.Ribosome
     RNA = "Ag" + chr(10) + chr(15) + "m" * 15 + "rrrg" + chr(10) + chr(15) + "Q"
 
     DNA = bitLibrary.functions._convertRNA(RNA)
@@ -60,74 +75,21 @@ def runGraphics():
         bits.Water(random.randrange(WORLD_WIDTH),
                        random.randrange(WORLD_HEIGHT))
 
-##    dna = """
-##/w 100 /s if(self.signature('GrowthTissueDecay')>=10):self.goto('A'); /hCytoDissolve /q
-##"""
-##
-##    rna = " " + \
-##          'f'*15+'r'+'f'*15 + 'r' + 'f'*15 + 'r' + 'f'*15 + 'r' + 'f'*15 + 'r' + 'f'*10 + 'rf' + 'y'*50 + 'c' + \
-##          'c' + 'c' + 'y'*50 + 'c' + 'y'*50 + 'c' + 'y'*400 + 'q' + \
-##          'd'*10 + dna
-
     screen.fill((255,255,255))
-
     engine.start()
-
-def runConsole():
-    METRIC_WIDTH = 10
-    METRIC_HEIGHT = 10
-    METRIC_T = 3
-    
-    hexmech.setMetrics(
-        width = 10, height = 10, t = 3)
-
-    WORLD_WIDTH = 200
-    WORLD_HEIGHT = 100
-
-    world = life.World(WORLD_WIDTH, WORLD_HEIGHT)
-
-    life.Bit.world = world
-
-    random.seed(0)
-
-    rna = " " + \
-          'f'*15+'r'+'f'*15 + 'r' + 'f'*15 + 'r' + 'f'*15 + 'r' + 'f'*15 + 'r' + 'f'*10 + 'rf' + 'y'*50 + 'c' + \
-          'c' + 'c' + 'y'*50 + 'c' + 'y'*50 + 'c' + 'y'*400 + 'q' + \
-          'd'*10 + 'g/w100 /hCytoDissolve /w100 /q'
-
-    bits.Ribosome(WORLD_WIDTH//2, WORLD_HEIGHT//2, rna, 0)
-    
-    for i in range(50):
-        bits.AminoAcid(random.randrange(60,110), random.randrange(20, 50))
-    for i in range(5):
-        bits.Oxidizer(random.randrange(world.width), random.randrange(world.height))
-    for i in range(1):
-        bits.StrongAcid(random.randrange(world.width), random.randrange(world.height))
-
-    printCounter = 0
-    for i in range(1):
-        if printCounter == 0:
-            printCounter = 50
-            print("ITERATION %-5d   BIT COUNT = %-5d" % (i, len(world.bits)))
-        world.tick()
-        printCounter -= 1
-
-    input()
 
 def main(profile = False):
 
-    statement = "runGraphics()"
-
     if profile:
         import cProfile
-        cProfile.run(statement)
+        cProfile.run(INIT_STATEMENT)
         input()
     else:
-        exec(statement)
+        exec(INIT_STATEMENT)
 
 if __name__ == "__main__":
     try:
-        main( False )
+        main( USE_PROFILER )
     except:
         print(traceback.format_exc())
         input()
