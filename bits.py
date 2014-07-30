bbabadc9d1c6d3bc85f5f456d7f1d8899ef3430e
Matthew Robinson <omegachysis@outlook.com>
2014-07-30 16:19:34 -0400

    Small cleanup

--- bits.py
+++ bits.py
@@ -4,14 +4,7 @@ import life
 
 from bitLibrary.experiments import *
 from bitLibrary.chemicals import *
-##from bitLibrary.hormones import *
-##from bitLibrary.membranes import *
 from bitLibrary.organelles import *
-##from bitLibrary.signatures import *
-##from bitLibrary.tests import *
-##from bitLibrary.tissues import *
 from bitLibrary.proteins import *
-##
-##from bitLibrary.mods import *
 
 from bitLibrary import functions
