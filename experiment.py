6132872f3c9c46a61cbe7b4ca403ae3c75ac96d8
Matthew Robinson <omegachysis@outlook.com>
2014-07-30 16:07:00 -0400

    Documented experiment.py

--- experiment.py
+++ experiment.py
@@ -1,5 +1,11 @@
 
+"""
+Provides access to the functions needed to monitor the details 
+of a given experiment.
+"""
+
 def probeAtoms(world):
+    """Return the total number of each atom in the world."""
     atoms = []
     for bit in world.bits:
         if len(bit.atoms) > len(atoms):
@@ -10,12 +16,14 @@ def probeAtoms(world):
     return atoms
 
 def probeEnthalpy(world):
+    """Return the total amount of enthalpy in the world."""
     total = 0
     for bit in world.bits:
         total += bit.enthalpy
     return total
 
 def probeEntropy(world):
+    """Return the total amount of entropy in the world."""
     total = 0
     for bit in world.bits:
         total += bit.ENTROPY
