d235fb480c995d109842b4ac9e6974c33c361c82
Matthew Robinson <omegachysis@outlook.com>
2014-09-14 16:01:28 -0400

    Created toolbox status bar icon with tooltip

--- biohex/ui.py
+++ biohex/ui.py
@@ -12,15 +12,18 @@ class UI(object):
         self.engine = engine
         self.pygameDisplay = engine.pygameDisplay
 
-        self.font = pygame.freetype.Font("assets/consola.ttf", 15)
+        self.font = pygame.freetype.Font("assets/consola.ttf", 13)
+        self.fontIcon = pygame.freetype.Font("assets/fontawesome.ttf", 13)
 
-        self.statusBarTextY = self.engine.screen.height - UI.HEIGHT // 2
+        self.STATUS_BAR_TEXT_Y = self.engine.screen.height - UI.HEIGHT // 2
+
+        self.statusHelperText = ""
 
     def _renderStatusValue(self, value, posPercent):
         valueSurface, valueRect = self.font.render(
             value, UI.STATUS_TEXT_COLOR, UI.STATUS_BACKGROUND_COLOR)
 
-        valueRect.centery = self.statusBarTextY
+        valueRect.centery = self.STATUS_BAR_TEXT_Y
         valueRect.left = self.engine.screen.width * posPercent
 
         self.engine.screen.surface.blit(valueSurface, valueRect)
@@ -31,12 +34,25 @@ class UI(object):
                                         (0, self.engine.screen.height-UI.HEIGHT,
                                          self.engine.screen.width, UI.HEIGHT))
 
+        wrenchSurface, wrenchRect = self.fontIcon.render(
+            "\uF0AD", UI.STATUS_TEXT_COLOR, UI.STATUS_BACKGROUND_COLOR)
+        wrenchRect.centery = self.STATUS_BAR_TEXT_Y
+        wrenchRect.right = self.engine.screen.width - 5
+        self.engine.screen.surface.blit(wrenchSurface, wrenchRect)
+
         self._renderStatusValue("\u03A3 ENTHALPY = " + str(self.engine.world.experiment.probeEnthalpy()),
-                                0.40)
+                                0.20)
         self._renderStatusValue("\u03A3 ENTROPY = " + str(self.engine.world.experiment.probeEntropy()),
-                                0.70)
-        self._renderStatusValue("TIME = " + str(self.engine.world.tickNumber), 0.10)
+                                0.35)
+        self._renderStatusValue("TIME = " + str(self.engine.world.tickNumber), 0.05)
 
+        mousex, mousey = pygame.mouse.get_pos()
+        if mousex > wrenchRect.left and mousex < wrenchRect.right and \
+            mousey > self.engine.screen.height-UI.HEIGHT and \
+            mousey < self.engine.screen.height:
+            self.statusHelperText = "Toggle Toolbox (t)"
+        else:
+            self.statusHelperText = ""
 
-
+        self._renderStatusValue(self.statusHelperText, 0.70)
 
